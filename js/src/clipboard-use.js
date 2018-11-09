832f70c6859b7a0e11956f3663e66c1fbc66044e
Orzsxy.github.io <921560202@qq.com>
2018-11-09 15:01:49 +0800

    Site updated: 2018-11-09 15:01:30

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,22 +1,18 @@
-/*页面载入完成后，创建复制按钮*/
-!function (e, t, a) { 
-  /* code */
-  var initCopyCode = function(){
-    var copyHtml = '';
-    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
-    copyHtml += '</button>';
-    $(".highlight .code pre").before(copyHtml);
-    new ClipboardJS('.btn-copy', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
-    });
-  }
-  initCopyCode();
-}(window, document);
-
-作者：恬雅过客
-链接：https://www.jianshu.com/p/3e9d614c1e77
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+  /*页面载入完成后，创建复制按钮*/
+  !function (e, t, a) { 
+    /* code */
+    var initCopyCode = function(){
+      var copyHtml = '';
+      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+      //fa fa-globe可以去字体库替换自己想要的图标
+copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
+      copyHtml += '</button>';
+      $(".highlight .code pre").before(copyHtml);
+      new ClipboardJS('.btn-copy', {
+          target: function(trigger) {
+              return trigger.nextElementSibling;
+          }
+      });
+    }
+    initCopyCode();
+  }(window, document);
